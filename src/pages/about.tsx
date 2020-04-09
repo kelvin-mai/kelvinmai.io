610d4e2de004e01fcebf8401b571661a1173829e
Kelvin Mai <kelvin.mai002@gmail.com>
2020-04-09 11:31:30 -0700

    feat: added patreon link

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,6 +7,7 @@ import { Button } from '../components/button';
 import { AboutSection } from '../components/about/about-section';
 import { Experience } from '../components/about/experience';
 import { Resume } from '../components/resume/resume';
+import { SocialLinks } from '../components/social-links';
 
 export const AnimateTransition = ({ children }) => {
   const variants = {
@@ -39,6 +40,12 @@ export const About = () => {
         <AboutSection />
       </Section>
       <Section className='bg-white text-black'>
+        <div className='w-full lg:w-2/3 mx-auto pb-2 shadow bg-real-white'>
+          <h3 className='text-center text-xl font-bold uppercase text-dark-purple'>
+            Follow Me
+          </h3>
+          <SocialLinks className='flex justify-around' />
+        </div>
         <Button onClick={toggleResume}>View as Resume</Button>
         <AnimatePresence exitBeforeEnter>
           <AnimateTransition key={resume ? 'resume' : 'experience'}>
