730b43cacc2339dc1f8f71a3aa76f7b009a189e6
Kelvin Mai <kelvin.mai002@gmail.com>
2020-04-30 22:56:07 -0700

    feat: updated navbar

--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import classnames from 'classnames';
-import { useToggle } from 'react-use';
-import { motion, AnimatePresence } from 'framer-motion';
+import React, { useRef } from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
+import { motion, useCycle } from 'framer-motion';
+import { useWindowSize } from 'react-use';
 
 import { Burger } from './burger';
 
@@ -17,45 +16,97 @@ const query = graphql`
   }
 `;
 
-export const Nav = () => {
-  const [open, toggleOpen] = useToggle(false);
-  const data = useStaticQuery(query);
-  const menu = {
-    closed: { opacity: 0, scaleX: 0, x: 24 },
-    open: { opacity: 1, scaleX: 1, x: 0 },
-    style: { originX: 1 },
+const sidebar = {
+  open: (height = 1000) => ({
+    clipPath: `circle(${height * 2 + 200}px at 30px 30px)`,
+    transition: {
+      type: 'spring',
+      stiffness: 20,
+      restDelta: 2,
+    },
+  }),
+  closed: {
+    clipPath: 'circle(24px at 30px 30px)',
+    transition: {
+      delay: 0.5,
+      type: 'spring',
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
+
+export const NavItem = ({ to, name }) => {
+  const variants = {
+    open: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        y: { stiffness: 1000, velocity: -100 },
+      },
+    },
+    closed: {
+      y: 50,
+      opacity: 0,
+      transition: {
+        y: { stiffness: 1000 },
+      },
+    },
   };
-  const menuItems = data.pages.nodes.map(page => (
-    <li key={page.name} className='ml-2 hover:text-purple'>
-      <Link to={page.to}>{page.name}</Link>
-    </li>
-  ));
   return (
-    <nav className='flex justify-center md:justify-end'>
-      <ul className='block flex item-center md:hidden'>{menuItems}</ul>
-      <AnimatePresence>
-        {open && (
-          <motion.ul
-            initial='closed'
-            animate='open'
-            exit='closed'
-            variants={menu}
-            style={menu.style}
-            className='flex items-center bg-black px-2 shadow-md rounded-md'
-          >
-            {menuItems}
-          </motion.ul>
-        )}
-      </AnimatePresence>
-      <div
-        className={classnames(
-          'hidden m-1 mx-4 md:block md:rounded-full md:shadow-xl md:p-2',
-          { ['md:bg-red']: open, ['md:bg-pink']: !open }
-        )}
-        onClick={toggleOpen}
-      >
-        <Burger open={open} />
+    <motion.li
+      className='ml-4 mb-4 flex items-center cursor-pointer'
+      variants={variants}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.95 }}
+    >
+      <Link className='pointer-cursor' to={to}>
+        {name}
+      </Link>
+    </motion.li>
+  );
+};
+
+export const NavItems = ({ items }) => {
+  const variants = {
+    open: { transition: { staggerChildren: 0.07, delayChildren: 0.2 } },
+    closed: { transition: { staggerChildren: 0.05, staggerDirection: -1 } },
+  };
+  return (
+    <motion.ul
+      className='absolute p-4 text-purple'
+      style={{ top: '50px', width: '230px' }}
+      variants={variants}
+    >
+      {items.map(item => (
+        <NavItem to={item.to} name={item.name} />
+      ))}
+    </motion.ul>
+  );
+};
+
+export const Nav = () => {
+  const [isOpen, toggleOpen] = useCycle(false, true);
+  const data = useStaticQuery(query);
+  const containerRef = useRef(null);
+  const { height } = useWindowSize();
+  return (
+    <motion.nav
+      className='absolute top-0 left-0 bottom-0'
+      style={{ width: '300px' }}
+      initial={false}
+      animate={isOpen ? 'open' : 'closed'}
+      ref={containerRef}
+    >
+      <motion.div
+        className='absolute bg-black top-0 left-0 bottom-0 shadow-lg'
+        style={{ width: '300px', height }}
+        variants={sidebar}
+      />
+      <NavItems items={data.pages.nodes} />
+      <div style={{ margin: '15px' }} onClick={() => toggleOpen()}>
+        <Burger open={isOpen} />
       </div>
-    </nav>
+    </motion.nav>
   );
 };
