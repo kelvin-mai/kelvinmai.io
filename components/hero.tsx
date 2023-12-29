e228470d36d169b0ce7f2124382b1e6cb466a292
Kelvin Mai <kelvin.mai002@gmail.com>
2023-12-28 16:33:59 -0800

    background image optimization

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,11 +1,19 @@
+import Image from 'next/image';
+
 import { SocialLinks } from './social-links';
 import { ResumeDownloadLink } from './resume';
 
 export const Hero = () => {
   return (
     <main
-      className={`flex min-h-[80vh] flex-col items-center justify-center bg-waikawa-950 bg-[url('/images/bg.jpg')] bg-fixed bg-bottom bg-no-repeat lg:min-h-[720px] lg:bg-top`}
+      className={`relative flex min-h-[80vh] flex-col items-center justify-center bg-waikawa-950 lg:min-h-[720px]`}
     >
+      <Image
+        layout='fill'
+        className='pointer-events-none object-cover object-center'
+        src='/images/bg.jpg'
+        alt=''
+      />
       <div className='container flex flex-col items-center justify-center gap-4 rounded-xl bg-gray-400 bg-opacity-10 bg-clip-padding px-4 py-16 drop-shadow backdrop-blur-sm backdrop-filter'>
         <h1 className='text-5xl font-bold tracking-tight text-white sm:text-[5rem]'>
           Kelvin Mai
