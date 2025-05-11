a8aa639c36246a57c4f877a17a5b7dc1a69c11d9
Kelvin Mai <kelvin.mai002@gmail.com>
2025-05-11 03:03:37 -0700

    fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,14 +1,16 @@
-import { MetadataRoute } from 'next';
+import { MetadataRoute } from "next";
 
-import { getBaseUrl } from '@/lib/utils';
+import { getBaseUrl } from "@/lib/utils";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
     {
-      url: `${getBaseUrl}/`,
+      url: `${getBaseUrl()}/`,
       lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
+    },
+    {
+      url: `${getBaseUrl()}/resume`,
+      lastModified: new Date(),
     },
   ];
 }
